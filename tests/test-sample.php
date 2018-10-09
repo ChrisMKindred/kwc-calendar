260887fdcbf9a4b4bc8e71cf23edcf935a1a3d55
Chris Kindred <CMKindred@gmail.com>
2018-10-09 08:00:40 -0400

    test2

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -9,9 +9,19 @@
  * Sample test case.
  */
 class SampleTest extends WP_UnitTestCase {
+	/**
+	 * The main plugin variable
+	 *
+	 * @var KWC_CALENDAR
+	 */
 	protected $plugin;
 
-	public function setUp(){
+	/**
+	 * Setup the testing environment
+	 *
+	 * @return void
+	 */
+	public function setUp() {
 		parent::setUp();
 		$this->plugin = new kwc_calendar();
 		$this->plugin->run();
@@ -19,28 +29,23 @@ class SampleTest extends WP_UnitTestCase {
 
 	/**
 	 * Tests for plugin slug.
+	 *
 	 * @covers kwc_calendar::get_kwc_calendar
 	 */
 	public function test_plugin_slug() {
-		// Replace this with some actual testing code.
 		$plugin_slug = $this->plugin->get_kwc_calendar();
-		
 		$this->assertEquals( 'kwc-calendar', $plugin_slug );
 	}
-	
+
 	/**
 	 * Tests for plugin slug.
+	 *
 	 * @covers kwc_calendar_Admin::__construct
 	 * @covers kwc_calendar_Admin::enqueue_scripts
 	 */
 	public function test_enqueue_scripts() {
-		// Replace this with some actual testing code.
 		$plugin_admin = new kwc_calendar_Admin( $this->plugin->get_kwc_calendar(), $this->plugin->get_version() );
-		var_dump( has_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' ) );
-				
-		var_dump( array( $this->plugin->get_kwc_calendar(), 'admin_enqueue_scripts' ) );
-		var_dump( has_action( 'enqueue_scripts', array( $this->plugin->get_kwc_calendar(), 'admin_enqueue_scripts' ) ) );
-		$this->assertGreaterThan(0, has_action( 'enqueue_scripts', array( $this->plugin->get_kwc_calendar(), 'admin_enqueue_scripts' ) ) );
+		var_dump( has_action( 'enqueue_scripts', array( $plugin_admin, 'admin_enqueue_scripts' ) ) );
+		$this->assertGreaterThan( 0, has_action( 'enqueue_scripts', array( $plugin_admin, 'admin_enqueue_scripts' ) ) );
 	}
-	
 }
